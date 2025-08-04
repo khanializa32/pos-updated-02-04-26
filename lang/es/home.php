edb2831da72c5fcef158d405e7e1468f0d09a666
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-03 20:49:37 -0500

    Se actualizaron algunas traducciones

--- lang/es/home.php
+++ lang/es/home.php
@@ -2,7 +2,7 @@
 
  return [
      'home' => 'Inicio', /* modified */
-     'welcome_message' => 'Bienvenido a Zisco :name',
+     'welcome_message' => 'Bienvenido a Zisco Plus:name',
      'total_sell' => 'Total Ventas',
      'total_purchase' => 'Total Compras', /* modified */
      'invoice_due' => 'Cuentas Por Cobrar',
@@ -11,10 +11,10 @@
      'this_week' => 'Esta Semana',
      'this_month' => 'Este Mes',
      'this_fy' => 'Este Año', /* modified */
-     'sells_last_30_days' => 'Ventas últimos 30 días',
-     'sells_current_fy' => 'Año actual de ventas', /* modified */
+     'sells_last_30_days' => 'Ventas día a día (Este Mes)',
+     'sells_current_fy' => 'Ventas Año Actual', /* modified */
      'total_sells' => 'Total Ventas (:currency)',
-     'product_stock_alert' => 'Alerta de stock del producto',
+     'product_stock_alert' => 'Alerta de Bajas Existencias',
      'payment_dues' => 'Pagos Pendientes',
      'due_amount' => 'Valor Adeudado',
      'stock_expiry_alert' => 'Alerta de Caducidad',
