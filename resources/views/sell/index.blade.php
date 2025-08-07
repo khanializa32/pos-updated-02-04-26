5a0144cd5f1f6b204036b5458aa6d56538c931f5
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-07 13:31:21 -0500

    Cambio en Traduccions

--- resources/views/sell/index.blade.php
+++ resources/views/sell/index.blade.php
@@ -99,18 +99,18 @@
                             <th>@lang('messages.date')</th>
                             <th>@lang('sale.invoice_no')</th>
                             <th>DIAN</th>
-                            <th>@lang('sale.customer_name')</th>
-                            <th>@lang('lang_v1.contact_no')</th>
+                            <th>@lang('Cliente')</th>
+                            <th>@lang('Celular')</th>
                             <th>@lang('sale.location')</th>
                             <th>@lang('sale.payment_status')</th>
-                            <th>@lang('lang_v1.payment_method')</th>
-                            <th>@lang('sale.total_amount')</th>
-                            <th>@lang('sale.total_paid')</th>
-                            <th>@lang('sale.utility_per_invoice')</th>
+                            <th>@lang('Pago en')</th>
+                            <th>@lang('Total')</th>
+                            <th>@lang('Pagado')</th>
+                            <th>@lang('Utilidad')</th>
                             <th>@lang('lang_v1.sell_due')</th>
-                            <th>@lang('lang_v1.sell_return_due')</th>
-                            <th>@lang('lang_v1.shipping_status')</th>
-                            <th>@lang('lang_v1.total_items')</th>
+                            <th>@lang('Devolución')</th>
+                            <th>@lang('Envío')</th>
+                            <th>@lang('items')</th>
                             <th>@lang('lang_v1.types_of_service')</th>
                             <th>{{ $custom_labels['types_of_service']['custom_field_1'] ?? __('lang_v1.service_custom_field_1') }}
                             </th>
