d36c224e3f3576b7b83d56e0aed70098606f816f
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-07 14:52:43 -0500

    Se cambio un boton por otro

--- resources/views/sale_pos/index.blade.php
+++ resources/views/sale_pos/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', __( 'sale.list_pos'))
+@section('title', __( 'Documentos Electrónicos ZISCO'))
 
 @section('content')
 
@@ -222,15 +222,15 @@
         @can('sell.create')
             @slot('tool')
                 <div class="box-tools">
-                    <a class="tw-dw-btn tw-bg--to-r tw-from-indigo-600 tw-to-blue-500 tw-font-bold tw-text-black tw-border-none tw-rounded-full pull-right"
-                            href="{{action([\App\Http\Controllers\SellPosController::class, 'create'])}}">
+                        <a class="tw-dw-btn tw-bg--to-r tw-from-indigo-600 tw-to-blue-500 tw-font-bold tw-text-black tw-border-none tw-rounded-full pull-right"
+                            href="{{ action([\App\Http\Controllers\SellController::class, 'create']) }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
                                         viewBox="0 0 20 20" fill="none" stroke="red" stroke-width="2" stroke-linecap="round"
                                             stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <path d="M12 5l0 14" />
                                             <path d="M5 12l14 0" />
-                                        </svg> @lang('Ir al POS')
+                                        </svg> @lang('Crear Docuemnto Electrónico')
                         </a>
                 </div>
             @endslot
