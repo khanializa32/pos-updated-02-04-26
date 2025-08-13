8f3683733d498d8dd2bf011e994fb1cb00be68b7
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-13 14:02:33 -0500

    Se actualizaron algunos colores de iconos

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -214,14 +214,14 @@
                     <div class="nav-tabs-custom">
                         <ul class="nav nav-tabs">
                             <li class="active">
-                                <a href="#product_list_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-cubes"
-                                        aria-hidden="true"></i> @lang('lang_v1.all_products')</a>
+                                <a href="#product_list_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-cubes" style=" font-size:24px ;color:orange"
+                                        aria-hidden="true"></i> @lang('Mis Productos')</a>
                             </li>
                             @can('stock_report.view')
                                 <li>
                                     <a href="#product_stock_report" class="product_stock_report" data-toggle="tab"
-                                        aria-expanded="true"><i class="fa fa-hourglass-half" aria-hidden="true"></i>
-                                     @lang('report.stock_report')</a>
+                                        aria-expanded="true"><i class="fas fa-chart-line" style=" font-size:24px ;color:lightseagreen" aria-hidden="true"></i>
+                                     @lang('Valor del Inventario')</a>
                                 </li>
                             @endcan
                         </ul>
