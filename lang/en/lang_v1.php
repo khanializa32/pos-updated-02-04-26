20c8e3a11b95da62e8b04801621b90e2f0b8c0eb
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-16 20:22:05 -0500

    A translation into Spanish was made.

--- lang/en/lang_v1.php
+++ lang/en/lang_v1.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'cost_price_validation_error' => 'El precio de venta no puede ser menor que el precio de compra (:cost_price)',
-    'cost_price_warning_title' => 'Advertencia de Precio',
-    'cost_price_warning_message' => 'El precio de venta es menor que el precio de compra. ¿Está seguro de que desea vender a este precio?',
+    'cost_price_validation_error' => 'Precio de Venta no puede ser menor al de Compra (:cost_price)',
+    'cost_price_warning_title' => 'Alerta',
+    'cost_price_warning_message' => 'Perderás si vendes a este precio, Deseas vender a este precio?',
     'cost_price_warning_accept' => 'Aceptar',
     'cost_price_warning_cancel' => 'Cancelar',
 ];
