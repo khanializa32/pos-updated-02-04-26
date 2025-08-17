cd96438516231f96f209c327269ab9998418b9b7
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-16 19:08:40 -0500

    Se actualizo a idioma a español

--- public/js/lang/es.js
+++ public/js/lang/es.js
@@ -116,9 +116,10 @@ LANG = {
 'authentication_successfull': 'Autenticación exitosa',
 'please_enter' : 'Por favor, ingrese',
 'or_more_characters' : 'o más caracteres',
-"cost_price_validation_error": "El precio de venta no puede ser menor que el precio de costo",
-"cost_price_warning_title": "Advertencia de Precio",
-"cost_price_warning_message": "El precio de venta es menor que el precio de costo. ¿Está seguro de que desea vender a este precio?",
-"cost_price_warning_accept": "Aceptar",
-"cost_price_warning_cancel": "Cancelar",
+'cost_price_validation_error': 'Precio de Venta no puede ser menor al de Compra',
+'cost_price_warning_title': 'Alerta',
+'cost_price_warning_message': 'Perderás si vendes a este precio, Deseas vender a este precio?',
+'cost_price_warning_accept': 'Aceptar',
+'cost_price_warning_cancel': 'Cancelar',
+
 };
