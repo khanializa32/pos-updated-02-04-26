372f715b5197b0cf496d85a6835198245107026a
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-08-23 20:49:56 -0500

    Se añadio boton Mis Llaves

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -223,7 +223,7 @@
                             
 
                         <li>
-                           <a href="https://test.ziscoplus.com/business-location/" 
+                           <a href="https://sistema.ziscoplus.com/business-location/" 
                                 class="tw-flex tw-items-center tw-gap-2 tw-px-3 tw-py-2 tw-text-sm tw-font-medium tw-text-gray-600 tw-transition-all tw-duration-200 tw-rounded-lg hover:tw-text-gray-900 hover:tw-bg-gray-100"
                                 role="menuitem" tabindex="-1">
                                 <i class='fas fa-key' style='font-size:20px;color:purple'></i>
