39d54f9212a0f2d403f49c2500a635a572410eae
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-09-03 11:57:45 -0500

    se elimino linea que no se va a necesitar

--- resources/views/business/partials/settings_tax.blade.php
+++ resources/views/business/partials/settings_tax.blade.php
@@ -54,41 +54,7 @@
                     [ 'class' => 'input-icheck']); !!} {{ __( 'lang_v1.enable_inline_tax' ) }}
                   </label>
                 </div>
-                
-                <!-- INICIO DE CODIGO SOLO PARA LA VISTA RETENCIONES (CONFIGURAR SEGUN IMAGEN), TAMBIEN PARA CONFIGURAR RETE ICA-->
-                
-        <div class="col-md-3"><h4>@lang('lang_v1.retention'):</h4></div>
-            </div>
             
-            <div class="col-sm-12">
-            <div class="form-group">
-                <div class="checkbox">
-                    <label>
-                        <input id="enable_rete_ica" name="enable_rete_ica" type="checkbox" value="1"> 
-                        Habilitar Rete ICA
-                    </label>
-                </div>
-            </div>
-        </div>
-                <!-- Campos adicionales de Rete ICA -->
-        <div id="rete_ica_fields" style="display: none;">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label for="valor_por_1000">Valor por 1000:*</label>
-                    <input class="form-control" id="valor_por_1000" name="valor_por_1000" type="text">
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label for="base_ica">Base de retenci&oacute;n ICA:*</label>
-                    <input class="form-control" id="base_ica" name="base_ica" type="text">
-                </div>
-            </div>
-            
-            
-          
-    <!-- FIN DE CODIGO SOLO PARA LA VISTA RETENCIONES (CONFIGURAR SEGUN IMAGEN), TAMBIEN PARA CONFIGURAR RETE ICA-->
             </div>
             
         </div>
