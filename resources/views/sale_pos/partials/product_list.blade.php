e8a570f07536b994fd26afe6cdd3ec607e9e80d2
Deliocds21cds <46601518+Deliocds21cds@users.noreply.github.com>
2025-09-04 22:49:27 -0500

    Se actualizo oara mostrar precio costo en lugar de precio venta en imagen pos

--- resources/views/sale_pos/partials/product_list.blade.php
+++ resources/views/sale_pos/partials/product_list.blade.php
@@ -17,7 +17,7 @@
 @forelse($products as $product)
     <div class="col-md-2 col-xs-4 product_list no-print">
         <div class="product_box hover:tw-shadow-lg hover:tw-animate-pulse" data-variation_id="{{ $product->id }}" data-enable-stock="{{ $product->enable_stock }}" data-initial-qty="{{ $product->qty_available }}"
-            title="{{ $product->name }} @if ($product->type == 'variable') - {{ $product->variation }} @endif {{ '(' . $product->sub_sku . ')' }} @if (!empty($show_prices)) @lang('lang_v1.default') - @format_currency($product->selling_price) @foreach ($product->group_prices as $group_price) @if (array_key_exists($group_price->price_group_id, $allowed_group_prices)) {{ $allowed_group_prices[$group_price->price_group_id] }} - @format_currency($group_price->price_inc_tax) @endif @endforeach @endif">
+            title="@if ($product->type == 'variable') - {{ $product->variation }} @endif  @if (!empty($show_prices))  @format_currency($product->purchase_price) @foreach ($product->group_prices as $group_price) @if (array_key_exists($group_price->price_group_id, $allowed_group_prices)) {{ $allowed_group_prices[$group_price->price_group_id] }} - @format_currency($group_price->price_inc_tax) @endif @endforeach @endif">
 
 
             <small class="text text-muted">{{$product->name}} 
